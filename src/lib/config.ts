be170df3a025c217518038c23c7789baa28c931d
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-06-09 14:23:03 +0200

    added tier price and date constants

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,8 @@
+export const YEAR = 2025;
+export const DATE = "October 16"
 export const GITHUB_URL = 'https://github.com/PyCon-NL/website';
 export const CONTACT_EMAIL = 'info@pycon-nl.org';
+export const PLATINUM_PRICE = "€9,000.00";
+export const GOLD_PRICE = "€5,500.00";
+export const SILVER_PRICE = "€3,000.00";
+export const BRONZE_PRICE = "€1,500.00";
