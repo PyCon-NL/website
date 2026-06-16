22bc0dac227c448f44414c6eddb5b174dfadfe7c
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-06-16 12:51:33 +0200

    updated instagram url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -9,12 +9,12 @@ export const CONFERENCE_VENUE = "Jaarbeurs Utrecht, Netherlands";
 export const TICKETS_URL = "/coming-soon";
 export const PAPERS_FORM_URL = '/coming-soon';
 export const VOLUNTEERS_FORM_URL = "/coming-soon";
-export const GITHUB_URL = 'https://github.com/PyCon-NL/website';
+export const GITHUB_URL = 'https://github.com/PyCon-NL/website/';
 export const PYNETHERLANDS_URL = 'https://www.pynl.org/';
 export const CODE_OF_CONDUCT_URL = 'https://www.pynl.org/code-of-conduct/';
 export const LINKEDIN_URL = 'https://www.linkedin.com/company/104324288/';
-export const INSTAGRAM_URL = '/coming-soon';
-export const MASTODON_URL = 'https://mastodon.social/@pycon_nl';
+export const INSTAGRAM_URL = 'https://www.instagram.com/pycon_nl/';
+export const MASTODON_URL = 'https://mastodon.social/@pycon_nl/';
 export const CONTACT_EMAIL = 'info@pycon-nl.org';
 
 // Call for papers and volunteers visibility (hides the item from the nav entirely)
