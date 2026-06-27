9e9419e74094a93b657bcd84b904fbdb36acd89c
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-06-27 12:18:33 +0200

    added channable to bronze tier

--- src/lib/data/2026/sponsors.ts
+++ src/lib/data/2026/sponsors.ts
@@ -1,3 +1,10 @@
 import type { Sponsor } from '../types';
 
-export const sponsors: Sponsor[] = [];
+export const sponsors: Sponsor[] = [
+    {
+        tier: "bronze",
+        name: "Channable",
+        logo: "/sponsors/channable_horizontal.png",
+        url: "https://www.channable.com/",
+    },
+];
