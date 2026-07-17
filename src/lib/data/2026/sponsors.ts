a0b4b1e2fb0936c972d9409333759e5022aef7ac
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-07-17 07:37:53 +0200

    added to 2026 sponsor

--- src/lib/data/2026/sponsors.ts
+++ src/lib/data/2026/sponsors.ts
@@ -2,6 +2,12 @@ import type { Sponsor } from '../types';
 
 export const sponsors: Sponsor[] = [
     {
+        tier: "gold",
+        name: "Qstars IT",
+        logo: "/sponsors/qstars.png",
+        url: "https://www.qstars.nl",
+    },
+    {
         tier: "bronze",
         name: "Channable",
         logo: "/sponsors/channable_horizontal.png",
