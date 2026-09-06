51125b5702fc4c8ed0bf2250b6a0b846e4670ba7
Derk Weijers <derk@weijers.xyz>
2026-09-06 19:02:03 +0200

    feat: initial 2026 schedule

--- src/lib/data/2026/schedule.ts
+++ src/lib/data/2026/schedule.ts
@@ -1,3 +1,75 @@
 import type { Schedule } from '../types';
 
-export const schedule: Schedule<2026> = [];
+export const schedule: Schedule<2026> = [
+  {
+    type: "other",
+    trackType: "common",
+    title: "Walk-in",
+    timeSlot: "08:00",
+  },
+  {
+    type: "other",
+    trackType: "common",
+    title: "Welcome",
+    timeSlot: "09:30",
+  },
+  {
+    type: "talk",
+    trackType: "common",
+    title: "Keynote - TBA",
+    description: ``,
+    room: "Polars",
+    speakerId: 99,
+    timeSlot: "09:45",
+    year: 2026,
+    id: 1,
+  },
+  {
+    type: "other",
+    trackType: "common",
+    title: "Break",
+    timeSlot: "10:30",
+  },
+  {
+    type: "talk",
+    trackType: "single",
+    title: "Community Organizers sprint",
+    description: "weuifhjwiuegfhui",
+    room: "Flash",
+    speakerId: 15,
+    timeSlot: "10:45",
+    year: 2026,
+    id: 2,
+    multiSlot: 2
+  },
+  {
+    type: "other",
+    trackType: "common",
+    title: "Lunch",
+    timeSlot: "12:35",
+  },
+  {
+    type: "other",
+    trackType: "common",
+    title: "Break",
+    timeSlot: "14:35",
+  },
+  {
+    type: "other",
+    trackType: "common",
+    title: "Break",
+    timeSlot: "15:55",
+  },
+  {
+    type: "other",
+    trackType: "common",
+    title: "Closing",
+    timeSlot: "16:55",
+  },
+  {
+    type: "other",
+    trackType: "common",
+    title: "Drinks & Snacks",
+    timeSlot: "17:05",
+  },
+];
