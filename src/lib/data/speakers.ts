fea839cce4e5cda6c5ce9ba12b35c77e3e7d50e6
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-09-15 10:37:31 +0200

    fixes build

--- src/lib/data/speakers.ts
+++ src/lib/data/speakers.ts
@@ -1,10 +1,10 @@
 import type { Speaker } from './types';
-// import { speakers as speakers2025 } from './2025/speakers';
+import { speakers as speakers2025 } from './2025/speakers';
 import { speakers as speakers2026 } from './2026/speakers';
 
 // Aggregates all years. Deduplicates by ID so that a speaker who returns
 // in a future year gets a merged years[] and a single profile URL.
-const allYearSpeakers = [...speakers2026];
+const allYearSpeakers = [...speakers2025, ...speakers2026];
 
 const speakerMap = new Map<number, Speaker>();
 for (const speaker of allYearSpeakers) {
